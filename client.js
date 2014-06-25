516a2163b90e525a3a65c6fb7e61062c23da607e
Tomasz Janczuk <tomasz@janczuk.org>
2014-06-25 00:11:50 -0700

    support for built-in functions

--- client.js
+++ client.js
@@ -1,14 +1,32 @@
 (function (id) {
+
+    var BuiltIns = function () {}
+
+    BuiltIns.prototype.ls = function () {
+        return navigator.userAgent;
+    };
+
+    var builtins = new BuiltIns();
     var address = 'ws://' + window.location.host + '/' + id;
     var socket = new WebSocket(address, 'server');
 
     socket.onmessage = function (msg) {
         try {
-            var k = eval.call(window, msg.data);
+            var data = typeof msg.data === 'string' ? msg.data.trim() : '';
+            var k;
+            if (data[0] === '.') {
+                if (data[data.length - 1] !== ')' && data[data.length - 1] !== ';')
+                    data += '()';
+                k = eval('builtins' + data);
+            }
+            else {
+                k = eval.call(window, data);
+            }
+
             socket.send(k + '');
         }
         catch (e) {
-            socket.send('Error:\n' + e);
+            socket.send('ERROR:\n' + e);
         }
     };
 
